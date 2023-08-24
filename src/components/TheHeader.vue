472789c2c02f3ff511e8fd435a30e1477da34be7
MarcoImpellizzeri <impellizzeri.marco18@gmail.com>
2023-08-24 14:27:22 +0200

    initial commit

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <div class="container-fluid">
+        <img src="" alt="">
+        <h1>Yu-Gi-Oh Api</h1>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
