62402776471da2289d063a2c4d93d2341d0c8b7c
MarcoImpellizzeri <impellizzeri.marco18@gmail.com>
2023-08-25 18:05:05 +0200

    Select 1.0

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,11 +1,11 @@
 <script>
 export default {
-props:{
-    card: {
-        type: Object,
-        required: true,
+    props: {
+        card: {
+            type: Object,
+            required: true,
+        }
     }
-}
 }
 </script>
 
@@ -14,7 +14,7 @@ props:{
         <img :src="card.card_images[0].image_url" class="card-img-top" alt="card img">
         <div class="card-body text-center d-flex flex-column">
             <h5 class="card-title mb-3">{{ card.name }}</h5>
-            <p class="card-text mt-auto">{{ card.type }}</p>
+            <p class="card-text mt-auto">{{ card.archetype }}</p>
         </div>
     </div>
 </template>
---
+++ src/components/Select.vue
@@ -0,0 +1,44 @@
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            namesOptions: []
+        }
+    },
+    methods: {
+        fatchOptionSelect() {
+            const url = "https://db.ygoprodeck.com/api/v7/archetypes.php"
+
+            axios.get(url).then((response) => {
+                this.namesOptions = response.data;
+            })
+        }
+    },
+    mounted() {
+        this.fatchOptionSelect();
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <select class="form-select my-5">
+            <option>Selezione l'archetipo</option>
+            <option v-for="(nameOption, i) in namesOptions" :key="`'option'${i}`">
+                {{ nameOption.archetype_name }}
+            </option>
+        </select>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.form-select {
+    width: 20%;
+    background-color: black;
+    color: white;
+    border: 0;
+    box-shadow: none;
+}
+</style>
